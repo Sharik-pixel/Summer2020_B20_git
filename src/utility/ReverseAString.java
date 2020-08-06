00f21f543bd2c5de1e2ccda58e4f5d1ebc28a44c
Sharik-pixel <anaraturdumanbetova@gmail.com>
2020-08-05 20:31:07 -0400

    add ReverseAString.java

---
+++ src/utility/ReverseAString.java
@@ -0,0 +1,14 @@
+package utility;
+
+public class ReverseAString {
+
+
+    public static void main(String[] args) {
+
+        String a = "Anara";
+        System.out.println(a);
+
+    }
+
+    // Java --> avaJ
+}
--- src/utility/ZombieAttack.java
+++ src/utility/ZombieAttack.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 public class ZombieAttack {
 
+
+
         public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
             int[] inhabitants = new int[8];
@@ -32,5 +34,8 @@ public class ZombieAttack {
             System.out.println("---- EXTINCT ----");
         }
     }
+
+
+
 
 
